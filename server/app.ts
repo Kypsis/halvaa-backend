6a8a48acef4928056a7ba31e7bca5de9002b008d
Harry Sild <puhhbaer@hotmail.com>
2020-02-21 12:34:03 +0200

    finalize server

--- server/app.ts
+++ server/app.ts
@@ -1,16 +1,52 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
+import knex from "knex";
+import dotenv from "dotenv";
 
 import contactsRouter from "./contactsRouter";
+
+dotenv.config();
+
+export const db = knex({
+  client: "pg",
+  connection: {
+    connectionString: process.env.DATABASE_URL
+  }
+});
+
+db.schema
+  .createTable("contacts", table => {
+    table.increments("id");
+    table.string("name");
+    table.string("phonenumber");
+    table.string("email");
+  })
+  .then(() =>
+    db("contacts").insert([
+      {
+        name: "Tim Banana",
+        phonenumber: "12345678",
+        email: "timb@forscale.com"
+      },
+      { name: "Bob Builder", phonenumber: "87654321", email: "bob@builder.io" },
+      { name: "Joe Schmoe", phonenumber: "24688642", email: "cheap@cheese.co" },
+      {
+        name: "Alice Whodafis",
+        phonenumber: "13577531",
+        email: "waiting@twentyfouryears.ok"
+      }
+    ])
+  )
+  .catch(error => {
+    console.error(error.message);
+  });
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use(contactsRouter);
-
-/* app.get("*", (req: Request, res: Response): void => {
-  res.send("API response");
-}); */
 
 export default app;
--- server/contactsRouter.ts
+++ server/contactsRouter.ts
@@ -1,41 +1,50 @@
 import express, { Request, Response } from "express";
+import { db } from "./app";
 
 const router = express.Router();
 
 router.get("/api/contacts", (req: Request, res: Response): void => {
-  try {
-    res.status(200).send("All contacts");
-  } catch (error) {
-    console.error(error.message);
-    res.status(400).send(error.message);
-  }
+  db.select("*")
+    .from("contacts")
+    .then(contacts => {
+      res.status(200).send(contacts);
+    })
+    .catch(error => res.sendStatus(400).json(error.message));
 });
 
 router.post("/api/contacts", (req: Request, res: Response): void => {
-  try {
-    res.status(200).send("New contact");
-  } catch (error) {
-    console.error(error.message);
-    res.status(400).send(error.message);
-  }
+  const { name, phonenumber, email } = req.body;
+
+  db("contacts")
+    .insert({ name, phonenumber, email })
+    .then(() => {
+      res.sendStatus(200);
+    })
+    .catch(error => res.sendStatus(400).json(error.message));
 });
 
-router.put("/api/contacts/:id", (req: Request, res: Response): void => {
-  try {
-    res.status(200).send(`Changed contact: ${req.params.id}`);
-  } catch (error) {
-    console.error(error.message);
-    res.status(400).send(error.message);
-  }
+router.put("/api/contacts", (req: Request, res: Response): void => {
+  const { id, name, phonenumber, email } = req.body;
+
+  db("contacts")
+    .where({ id })
+    .update({ name, phonenumber, email })
+    .then(() => {
+      res.sendStatus(200);
+    })
+    .catch(error => res.sendStatus(400).json(error.message));
 });
 
-router.delete("/api/contacts/:id", (req: Request, res: Response): void => {
-  try {
-    res.status(200).send(`Deleted contact: ${req.params.id}`);
-  } catch (error) {
-    console.error(error.message);
-    res.status(400).send(error.message);
-  }
+router.delete("/api/contacts", (req: Request, res: Response): void => {
+  const { id } = req.body;
+
+  db("contacts")
+    .where({ id })
+    .del()
+    .then(() => {
+      res.sendStatus(200);
+    })
+    .catch(error => res.sendStatus(400).json(error.message));
 });
 
 export default router;
